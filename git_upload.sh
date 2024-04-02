a280338b6714aa3f9779d6e49bc50a06d15d01c4
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-04-02 12:03:06 -0500

    updated shell script

--- git_upload.sh
+++ git_upload.sh
@@ -6,20 +6,22 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=2g
 
-OUTPUT_FILE ="csci5541-s24-hw4-NLPitch-3*.json"
+OUTPUT_FILE="csci5541-s24-hw4-NLPitch-3*.json"
 OUTPUT_DIR="csci5541-hw-prompting"
 
-mv OUTPUT_FILE ../
+mv $OUTPUT_FILE ../
 cd ..
 
+if [ -d "$OUTPUT_DIR" ]; then
+    echo "$OUTPUT_DIR already exist."
+else
+    git clone https://github.com/minnesotanlp/$OUTPUT_DIR.git
+fi
 
-# if [ -d "$OUTPUT_DIR" ]; then
-#     echo "$OUTPUT_DIR already exist."
-# else
-#     git clone https://github.com/minnesotanlp/$OUTPUT_DIR.git
-# fi
+mv $OUTPUT_FILE $OUTPUT_DIR/
+cd $OUTPUT_DIR
 
-# cd OUTPUT_DIR
-
-# git branch NLPitch
-# git checkout NLPitch
+git branch NLPitch
+git checkout NLPitch
+git add --all
+git commit -m "NLPitch: Data file push"
